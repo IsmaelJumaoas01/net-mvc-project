923d0456d64086e156059edab9fbd001d330e5cb
IsmaelJumaoas01 <ismaeljumaoas2004@gmail.com>
2025-02-25 07:03:50 +0800

    25/2/2025

--- obj/Debug/net8.0/homeowner_app.AssemblyInfo.cs
+++ obj/Debug/net8.0/homeowner_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeowner_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b47e89982886ef6a742e058df61494063566dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581e9cc0702d512fd5bf8d061bd59a96ca9a0137")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeowner_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeowner_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
